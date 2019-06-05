ebd5c77ca046c8d0483d1c1ac3b9d047775ce1b1
liaowang <524342291@qq.com>
2019-06-05 11:23:10 +0800

    登录相关问题

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -77,8 +77,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
